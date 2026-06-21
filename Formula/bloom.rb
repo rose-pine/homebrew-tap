3ed956c6879e4a49fc56cca72d568fe59476a29e
mvllow <mvllow@icloud.com>
2026-06-21 18:46:59 -0500

    cleanup formula

--- Formula/bloom.rb
+++ Formula/bloom.rb
@@ -3,13 +3,12 @@ class Bloom < Formula
   homepage "https://github.com/rose-pine/rose-pine-bloom"
   url "https://github.com/rose-pine/rose-pine-bloom/archive/refs/tags/v3.1.0.tar.gz"
   sha256 "3e998e424adedc155b080153e4225e0b9c429a4ac76946396f85f26c3aacaf46"
-  head "https://github.com/rose-pine/rose-pine-bloom.git", branch: "main"
+  license "MIT"
 
   depends_on "go" => :build
 
   def install
-    ldflags = "-s -w -X github.com/rose-pine/rose-pine-bloom/cmd.version=#{version}"
-    system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"bloom"), "."
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/rose-pine/rose-pine-bloom/cmd.version=#{version}", output: bin/"bloom"), "."
     generate_completions_from_executable(bin/"bloom", "completion")
   end
 
